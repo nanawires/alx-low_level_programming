5d490ba7dedcec596cc4210f26c7115cb9a20a7d
nanawires <nanagyb@gmail.com>
2023-02-16 16:10:58 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a statement and followed by a new line
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
