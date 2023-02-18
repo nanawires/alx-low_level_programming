e7feefd2df2f145290850be272ce288f395bfd2f
nanawires <nanagyb@gmail.com>
2023-02-17 21:35:20 -0800

    alphabets with q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lower and uppercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char alphabts = 'a';
+
+	while (alphabts <= 'z')
+	{
+		if (alphabts != 'e' && alphabts != 'q')
+		{
+			putchar(alphabts);
+		}
+		alphabts++
+	}
+	putchar('\n');
+	return (0);
+}
