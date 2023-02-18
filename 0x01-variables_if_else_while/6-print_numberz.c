4d264d4c11c95b9a98454bb07414bef3cd1a9e31
nanawires <nanagyb@gmail.com>
2023-02-17 21:59:48 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numba;
+
+	for (numba = '0'; numba <= '9'; numba++)
+	putchar(numba);
+
+	putchar('\n');
+	return (0);
+}
