58b51acccde1d08f7d91d162b96af68b12a47435
nanawires <nanagyb@gmail.com>
2023-02-19 14:18:59 -0800

    combination of three digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+/**
+ * main - A program that prints different combinations of three digits
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int numba1, numba2, numba3;
+
+	for (numba1 = 0; numba1 < 8; numba1++)
+	{
+		for (numba2 = numba1 + 1; numba2 < 9; numba2++)
+		{
+			for (numba3 = numba2 + 1; numba3 < 10; numba3++)
+			{
+				putchar((numba1 % 10) + '0');
+				putchar((numba2 % 10) + '0');
+				putchar((numba3 % 10) + '0');
+
+				if (numba1 == 7 && numba2 == 8 && numba3 == 9)
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
