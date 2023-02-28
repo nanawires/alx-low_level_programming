d89e2accf414a03e5dfcac612db16253ba8c5bda
nanawires <nanagyb@gmail.com>
2023-02-27 18:08:15 -0800

    Great leaders are willing to sacrifice the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,41 +9,20 @@
  */
 int _atoi(char *s)
 {
-	int i, d, n, len, f, digit;
+	int sign = 1;
+	unsigned int num = 0;
 
+	do {
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+		if (*s == '-')
+			sign *= -1;
 
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
 
-	while (s[len] != '\0')
-	{
-		if (s[i] == '-')
-			++d;
+		else if (num > 0)
+			break;
+	} while (*s++);
 
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			 digit = s[i] - '0';
-			 if (d % 2)
-			 digit = -digit;
-			 n = n * 10 + digit;
-			 f = 1;
-			 if (s[i + 1] < '0' || s[i + 1] > '9')
-			 break;
-			 f = 0;
-		}
-		i++;
-	}
-
-
-	if (f == 0)
-		return (0);
-
-
-	return (n);
+	return (num * sign);
 }
