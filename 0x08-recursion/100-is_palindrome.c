fb36b21b1324994e0d4a0563735573c5c1cd11ea
nanawires <nanagyb@gmail.com>
2023-03-08 18:11:10 -0800

    function that returns 1 if a string is a palindrome and 0 if not

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * is_palindrome - check is the value is palindrome or not
+ * @s: check the string if its palindrome
+ * Return: return 1 if true and 0 if false
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strlen(s);
+
+	if (length <= 1)
+	{
+		return (1);
+	}
+	if (s[0] != s[length - 1])
+	{
+		return (0);
+	}
+	return (1);
+}
