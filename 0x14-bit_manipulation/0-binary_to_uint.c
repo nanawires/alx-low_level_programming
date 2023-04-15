3556d230c940feae307792fd9ec4bca949971d14
nanawires <nanagyb@gmail.com>
2023-04-15 11:11:33 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		dec = 2 * dec + (b[i] - '0');
+		val = 2 * val + (b[i] - '0');
 	}
 	return (val);
 }
