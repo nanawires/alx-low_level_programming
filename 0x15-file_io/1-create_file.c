34463e8f768f9b640e00efa0e1c4e0fe141269da
nanawires <nanagyb@gmail.com>
2023-04-20 05:51:52 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,9 @@ int create_file(const char *filename, char *text_content)
 {
 	int k = 0, file;
 
-	for (filename == NULL)
+	if (filename == NULL)
 		return (-1);
-	for (text_content == NULL)
+	if (text_content == NULL)
 		text_content = "";
 
 	while (text_content[k] != '\0')
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	for (file == -1)
+	if (file == -1)
 		return (-1);
 
 	write(file, text_content, k);
